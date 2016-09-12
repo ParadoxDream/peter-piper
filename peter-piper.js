47836777bb21eec3d3a84c802da9cf0916505f21
ParadoxDream <douglas.sheese@ucollege.edu>
2016-09-11 19:46:45 -0500

    first look

--- peter-piper.js
+++ peter-piper.js
@@ -22,4 +22,13 @@ var counter = 0;
 // TODO: Count the number of times an uppercase P or lowercase p character
 //       occurs in the `peterpiper` string.
 
+for (var i = 0; i < peterpiper.length; i++) {
+     if (peterpiper.charAt(i) == 'P') {
+         counter++;
+     }
+     if (peterpiper.charAt(i) == 'p') {
+         counter++;
+     }
+ }
+
 console.log("There are " + counter + " P's in Peter Piper.");
